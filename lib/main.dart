a1e8c00ac3e64761f6208ed8e375cfc2ee3a2abb
Usama Zafar <usamakzafar@gmail.com>
2025-08-10 16:20:40 +0200

    Status bar color update

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter/services.dart';
 import 'app.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
+
+  // Set status bar color and icon brightness
+  SystemChrome.setSystemUIOverlayStyle(
+    const SystemUiOverlayStyle(
+      statusBarColor: Color(0xFF004e4f), // Green
+      statusBarIconBrightness: Brightness.light, // White icons
+      statusBarBrightness: Brightness.dark, // For iOS
+    ),
+  );
+
   runApp(const ProviderScope(child: TripNesterApp()));
 }
